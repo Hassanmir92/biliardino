a7562498292c7bfecf15a92a64653719e91d1e90
Hassanmir92 <hassanmir92@gmail.com>
2015-11-20 13:59:17 +0000

    middlewear and using some of the modules

--- api/app.js
+++ api/app.js
@@ -16,4 +16,19 @@ var secret         = require('./config/config').secret;
 
 mongoose.connect(config.database);
 
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use(cors());
+app.use(passport.initialize());
+
 app.listen(3000);
