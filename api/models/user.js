f102d6c5898327abb31c4b39d8f931544d508633
Hassanmir92 <hassanmir92@gmail.com>
2015-11-20 14:11:14 +0000

    passport file created

--- api/models/user.js
+++ api/models/user.js
@@ -6,7 +6,8 @@ var userSchema = new mongoose.Schema({
     fullname: { type: String, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
-    role: { type: String, required: true } 
+    role: { type: String, required: true },
+    image: { type: String }
   }
 });
 
