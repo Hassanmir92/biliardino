ecc3862f71e5ed8cca665636f51840d64db1eb06
Hassanmir92 <hassanmir92@gmail.com>
2015-11-23 10:29:37 +0000

    doing clubs controller

--- api/config/routes.js
+++ api/config/routes.js
@@ -3,11 +3,14 @@ var router   = express.Router();
 var passport = require("passport");
 
 var usersController = require('../controllers/usersController');
+var clubsController = require('../controllers/clubsController');
 var authenticationsController = require('../controllers/authenticationsController');
 
+// Authentication routes
 router.post('/login', authenticationsController.login);
 router.post('/register', authenticationsController.register);
 
+// User routes
 router.route('/users')
   .get(usersController.usersIndex)
 
@@ -17,4 +20,25 @@ router.route('/users/:id')
   .patch(usersController.usersUpdate)
   .delete(usersController.usersDelete)
 
-module.exports = router
+// Club routes
+router.route('/clubs')
+  .get(clubsController.clubsIndex)
+  .post(clubsController.clubsPost)
+
+router.route('/clubs/:id')
+  .get(clubsController.clubsShow)
+  .put(clubsController.clubsUpdate)
+  .delete(clubsController.clubsDelete)
+
+router.route('/clubs/:id/vote')
+  .patch(clubsController.clubsAddVote)
+
+module.exports = router;
+
+
+
+
+
+
+
+
